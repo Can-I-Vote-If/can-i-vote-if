925d09764f41650085a5c24136b4f326c4f6b426
Jenessa White <jenessaw31@gmail.com>
2017-09-24 14:56:36 -0500

    it works...kinda

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
 			citizen: null,
 			age: null,
 			crimes: null,
+			submitted: false,
 		};
 
 		this.handleStateChange = this.handleStateChange.bind(this);
@@ -71,15 +72,15 @@ class App extends Component {
 	}
 
 	handleSubmit(event) {
+		let isSubmitted = this.state.submitted
+		this.state.submitted = true
 		event.preventDefault();
-		alert('Here is your info: ' + this.state);
 		console.log(this.state)
+		return isSubmitted
 	}
 
-	checkForm() {
-		alert("Hey there!")
-	}
 	render() {
+		let isSubmitted=this.state.submitted;
 		return (
 			<form id="form" onSubmit={this.handleSubmit}>
 				<div className="row">
@@ -93,57 +94,57 @@ class App extends Component {
 							<label htmlFor="where-do-you-live" className="select-label">Where do you live?<span className="required">*</span></label>
 							<select className="form-control" name="where-do-you-live" id="where-do-you-live" required="required" aria-required="true" defaultValue="Select State" onChange={this.handleStateChange}>
 								<option value="Select State" disabled>Select State</option>
-								<option value="AL">Alabama</option>
-								<option value="AK">Alaska</option>
-								<option value="AZ">Arizona</option>
-								<option value="AR">Arkansas</option>
-								<option value="CA">California</option>
-								<option value="CO">Colorado</option>
-								<option value="CT">Connecticut</option>
-								<option value="DE">Delaware</option>
-								<option value="DC">District Of Columbia</option>
-								<option value="FL">Florida</option>
-								<option value="GA">Georgia</option>
-								<option value="HI">Hawaii</option>
-								<option value="ID">Idaho</option>
-								<option value="IL">Illinois</option>
-								<option value="IN">Indiana</option>
-								<option value="IA">Iowa</option>
-								<option value="KS">Kansas</option>
-								<option value="KY">Kentucky</option>
-								<option value="LA">Louisiana</option>
-								<option value="ME">Maine</option>
-								<option value="MD">Maryland</option>
-								<option value="MA">Massachusetts</option>
-								<option value="MI">Michigan</option>
-								<option value="MN">Minnesota</option>
-								<option value="MS">Mississippi</option>
-								<option value="MO">Missouri</option>
-								<option value="MT">Montana</option>
-								<option value="NE">Nebraska</option>
-								<option value="NV">Nevada</option>
-								<option value="NH">New Hampshire</option>
-								<option value="NJ">New Jersey</option>
-								<option value="NM">New Mexico</option>
-								<option value="NY">New York</option>
-								<option value="NC">North Carolina</option>
-								<option value="ND">North Dakota</option>
-								<option value="OH">Ohio</option>
-								<option value="OK">Oklahoma</option>
-								<option value="OR">Oregon</option>
-								<option value="PA">Pennsylvania</option>
-								<option value="RI">Rhode Island</option>
-								<option value="SC">South Carolina</option>
-								<option value="SD">South Dakota</option>
-								<option value="TN">Tennessee</option>
-								<option value="TX">Texas</option>
-								<option value="UT">Utah</option>
-								<option value="VT">Vermont</option>
-								<option value="VA">Virginia</option>
-								<option value="WA">Washington</option>
-								<option value="WV">West Virginia</option>
-								<option value="WI">Wisconsin</option>
-								<option value="WY">Wyoming</option>
+								<option value="Alabama">Alabama</option>
+								<option value="Alaska">Alaska</option>
+								<option value="Arizona">Arizona</option>
+								<option value="Arkansas">Arkansas</option>
+								<option value="California">California</option>
+								<option value="Colorado">Colorado</option>
+								<option value="Connecticut">Connecticut</option>
+								<option value="Delaware">Delaware</option>
+								<option value="District of Columbia">District of Columbia</option>
+								<option value="Florida">Florida</option>
+								<option value="Georgia">Georgia</option>
+								<option value="Hawaii">Hawaii</option>
+								<option value="Idaho">Idaho</option>
+								<option value="Illinois">Illinois</option>
+								<option value="Indiana">Indiana</option>
+								<option value="Iowa">Iowa</option>
+								<option value="Kansas">Kansas</option>
+								<option value="Kentucky">Kentucky</option>
+								<option value="Louisiana">Louisiana</option>
+								<option value="Maine">Maine</option>
+								<option value="Maryland">Maryland</option>
+								<option value="Massachusetts">Massachusetts</option>
+								<option value="Michigan">Michigan</option>
+								<option value="Minnesota">Minnesota</option>
+								<option value="Mississippi">Mississippi</option>
+								<option value="Missouri">Missouri</option>
+								<option value="Montana">Montana</option>
+								<option value="Nebraska">Nebraska</option>
+								<option value="Nevada">Nevada</option>
+								<option value="New Hampshire">New Hampshire</option>
+								<option value="New Jersey">New Jersey</option>
+								<option value="New Mexico">New Mexico</option>
+								<option value="New York">New York</option>
+								<option value="North Carolina">North Carolina</option>
+								<option value="North Dakota">North Dakota</option>
+								<option value="Ohio">Ohio</option>
+								<option value="Oklahoma">Oklahoma</option>
+								<option value="Oregon">Oregon</option>
+								<option value="Pennsylvania">Pennsylvania</option>
+								<option value="Rhode Island">Rhode Island</option>
+								<option value="South Carolina">South Carolina</option>
+								<option value="South Dakota">South Dakota</option>
+								<option value="Tennessee">Tennessee</option>
+								<option value="Texas">Texas</option>
+								<option value="Utah">Utah</option>
+								<option value="Vermont">Vermont</option>
+								<option value="Virginia">Virginia</option>
+								<option value="Washington">Washington</option>
+								<option value="West Virginia">West Virginia</option>
+								<option value="Wisconsin">Wisconsin</option>
+								<option value="Wyoming">Wyoming</option>
 							</select>
 						</div>
 					</div>
@@ -177,7 +178,13 @@ class App extends Component {
 						<input id="submit" className="btn btn-primary" type="submit" value="Find Out Your Voting Laws"/>	
 					</div>
 				</div>
-				<Results />
+
+				{this.state.submitted ? (
+					<Results name={this.state.indState} age={this.state.age} citizen={this.state.citizen} crimes={this.state.crimes} />
+				) : (
+					null
+				)}
+
 			</form>
 		);
 	}
--- src/components/results.js
+++ src/components/results.js
@@ -1,39 +1,60 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import api from './api';
 
 export default class Results extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			data: {},
+		};
 	}
+
+	componentDidMount(props) {
+		console.log(this.props)
+		console.log('name: ' + this.props.name);
+		this.getStates()
+	}
+
+  getStates() {
+    axios.get(api() + '/api/states/' + this.props.name)
+		.then((resp) => {
+			console.log(resp);
+			this.setState({data: resp.data[0]})
+			console.log(this.state.data)
+		}).catch(function (error) {
+			console.log(error);
+		});
+  }
+	
 	render() {
+		const info = this.state.data
 		return (
-			<div id="results" className="row">
+			<div>
 				<div className="col-sm-12">
-					<h3 className="stateTitle">Can you vote in Minnesota? <span id="answer" class="yes">Yes</span></h3>
+					<h3 className="stateTitle">Can you vote in {this.props.name}? <span id="answer" className="yes">Yes</span></h3>
 				</div>
 				<div className="col-sm-2 col-sm-offset-1">
 					<label className="eTitle">Citizenship <i className="fa fa-check" id="us-citizen" aria-hidden="true"></i></label>
-					<p>Bacon ipsum dolor amet shoulder jerky shank porchetta. Tri-tip frankfurter bacon pork belly chicken ham salami bresaola
-						ribeye ball tip swine. Ball tip biltong shoulder tri-tip fatback frankfurter.</p>
+					<p>{info.citizenship}</p>
 				</div>
 				<div className="col-sm-2">
 					<label className="eTitle">Age <i className="fa fa-check" id="age-notice" aria-hidden="true"></i></label>
-					<p>Bacon ipsum dolor amet shoulder jerky shank porchetta. Tri-tip frankfurter bacon pork belly chicken ham salami bresaola
-						ribeye ball tip swine. Ball tip biltong shoulder tri-tip fatback frankfurter.</p>
+					<p>
+					{info.age}</p>
 				</div>
 				<div className="col-sm-2">
 					<label className="eTitle">Residency <i className="fa fa-check" id="resident" aria-hidden="true"></i></label>
-					<p>Bacon ipsum dolor amet shoulder jerky shank porchetta. Tri-tip frankfurter bacon pork belly chicken ham salami bresaola
-						ribeye ball tip swine. Ball tip biltong shoulder tri-tip fatback frankfurter.</p>
+					<p>{info.residency}</p>
 				</div>
 				<div className="col-sm-2">
 					<label className="eTitle">Crimes <i className="fa fa-times" id="criminal" aria-hidden="true"></i></label>
-					<p>Bacon ipsum dolor amet shoulder jerky shank porchetta. Tri-tip frankfurter bacon pork belly chicken ham salami bresaola
-						ribeye ball tip swine. Ball tip biltong shoulder tri-tip fatback frankfurter.</p>
+					<p>{info.criminalHistory}</p>
 				</div>
 				<div className="col-sm-2">
 					<label className="eTitle">Other <i className="fa fa-check" id="other" aria-hidden="true"></i></label>
-					<p>Bacon ipsum dolor amet shoulder jerky shank porchetta. Tri-tip frankfurter bacon pork belly chicken ham salami bresaola
-						ribeye ball tip swine. Ball tip biltong shoulder tri-tip fatback frankfurter.</p>
+					<p>{info.other}</p>
 				</div>
 			</div>
 		);
