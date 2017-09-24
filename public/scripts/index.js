abf3fa64e01d66203837c825d40342d909aa0473
Jenessa White <jenessaw31@gmail.com>
2017-09-23 23:03:24 -0500

    got some conditional rendering

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,22 +1,32 @@
 $(document).ready(function () {
-	$('#purpose').on('change', function () {
-		if (this.value == '1') {
-			$("#business").show();
+	$("select").change(function () {
+		$("input[type=text]").val($(this).val());
+	});
+
+	$('input[type="radio"]').on('click change', function (e) {
+		if (this.value == 'yes') {
+			$("#us-citizen").show();
 		}
 		else {
-			$("#business").hide();
+			$("#us-citizen").hide();
 		}
 	});
 
 	$("#birthdate").change(function () {
 		var today = new Date();
-		var birthDate = new Date($('#dob').val());
+		var birthDate = new Date($('#birthdate').val());
 		var age = today.getFullYear() - birthDate.getFullYear();
 		var m = today.getMonth() - birthDate.getMonth();
 		if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
 			age--;
 		}
-		return $('#age').html(age + ' years old');
+
+		if (age < 18) {
+			$('#age-notice').show();
+		}
+		else {
+			$("#age-notice").hide();
+		}
 	});
 
 });
