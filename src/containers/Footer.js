8fa81b5e66efc8107599f4ee1c0533acadf392e8
Jenessa White <jenessaw31@gmail.com>
2018-10-28 22:58:18 -0500

    clean up header and footer

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="row footer-wrap">
-      <p className="col-sm-12 text-center">
+    <footer className="container footer-wrap">
+      <p className="text-center">
         <strong>
           Find out how to register to vote in your state by following the link
           below.
         </strong>
+
+        <br />
+        
+        <a
+          href="https://vote.gov/"
+          target="_blank"
+          rel="noopener noreferrer">
+          Register at vote.gov
+        </a>
       </p>
-      <a
-        href="https://vote.gov/"
-        className="footer col-sm-12 parentButton"
-        target="_blank"
-        rel="noopener noreferrer">
-        Register at vote.gov
-      </a>
-    </div>
+    </footer>
   );
 };
 
---
+++ src/containers/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <nav class="navbar">
+        <Link to="/" className="navbar-brand"><h1>Can I Vote If?</h1></Link>
+
+        <ul className="nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about/" className="nav-link">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
