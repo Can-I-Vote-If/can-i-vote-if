90abdb47c0ae6d10be4aa8b8d826af73b6a72eb4
Jenessa White <jenessaw31@gmail.com>
2018-10-28 22:57:44 -0500

    restructure the app for router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Form from './components/Form';
+import Header from './containers/Header';
+import Home from './containers/Home';
+import About from './containers/About';
 import Footer from './containers/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Form />
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <React.Fragment>
+      <Header />
+      <main className="container-fluid pt-4">
+        <Route path="/" exact component={Home} />
+        <Route path="/about/" component={About} />
+      </main>
+      <Footer />
+    </React.Fragment>
+  </Router>
+);
 
 export default App;
